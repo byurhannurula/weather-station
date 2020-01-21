b5265bf25145936c5280064cc824d8ae615e2227
Byurhan Beyzat <imbyurhan@gmail.com>
2020-01-21 16:48:39 +0200

    Initial setup

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Weather Station 
+
+> *Weather station - temperature, humidity, air pressure.*
+
+### Weather station powered by Arduino and will consume energy from batteries which will be charged from solar panels.
+
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+*
+* Version: 0.0.1
+* Name: Arduino based Wetaher Station
+*
+* Author: Byurhan Beyzat / @byurhanbeyzat / https://byurhanbeyzat.com/
+*
+**/
+
+#include "Arduino.h"
+
+
+void setup() {
+  Serial.println("Hello World!");
+}
+
+void loop() {
+  
+}
